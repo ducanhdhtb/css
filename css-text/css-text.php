ba3984c89f8b38086cb976bf2bdf12f27e049db7
Duc Anh <ducanhdhtb@gmail.com>
2018-04-24 00:40:51 +0700

    css-font

---
+++ css-text/css-text.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+h1 {
+    letter-spacing: 3px;
+}
+
+h2 {
+    letter-spacing: -3px;
+}
+span.line-height{
+	line-height:11px;
+}
+</style>
+</head>
+<body>
+
+<h1>This is heading 1</h1>
+<h2>This is heading 2</h2>
+<span class = "line-height">
+	Line-height.<br>
+	Line-height.<br>
+</span>
+
+</body>
+</html>
