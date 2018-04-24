121f20d095e02c86f1208ff063f5dcee3cc64557
Duc Anh <ducanhdhtb@gmail.com>
2018-04-24 11:08:31 +0700

    css-link

---
+++ css-link/css-link-2.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+a:link, a:visited {
+   text-decoration:none;
+   background:red;
+   padding:15px 15px;
+   display:inline-block;
+   
+  
+}
+a:hover{
+	background:yellow;
+    color:white;
+}
+a:active{
+	background:black;
+    color:white;
+}
+
+
+
+</style>
+</head>
+<body>
+
+<a href="default.asp" target="_blank">This is a link</a>
+
+</body>
+</html>
